9a853821a9ed05b72d195f94bc3f33bc82c86472
deysaikat04 <30950554+deysaikat04@users.noreply.github.com>
2019-09-10 14:00:09 +0530

    Update recaptcha.php

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -2,6 +2,8 @@
 if ( ! defined('BASEPATH')) 
     exit('No direct script access allowed');
 
-$config['secret_key'] = '6LdVpkAUAAAAAD2OannStDKNsU_to4RzXBzQRJk2';
-$config['site_key'] = '6LdVpkAUAAAAALWm8RUt3mFDca6HqQ-SmoLv79bJ';
+$config['secret_key'] = '6LdVpkAUAAAAAD2OannStDKNsU_to4RzXBzQRk2';
+$config['site_key'] = '6LdVpkAUAAAAALWm8RUt3mFDca6HqQ-SmoLv7bJ';
 $config['verify_url'] = 'https://www.google.com/recaptcha/api/siteverify';
+
+/*XJ9*/
